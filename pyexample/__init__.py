2118a421d8063c7998d9b1fcd029af775079cb41
OAustlid <Oaustlid@gmail.com>
2022-12-01 10:55:55 +0100

    initial

---
+++ pyexample/__init__.py
@@ -0,0 +1,10 @@
+from .core import *
+
+def get_version():
+    return "0.0.1"
+
+def setup():
+    """
+    Do some configuration
+    """
+    print("Do some setup")
---
+++ pyexample/core.py
@@ -0,0 +1,7 @@
+from . import helpers
+
+def f(x):
+    return x
+
+def another_f():
+    print(helpers.get_str())
